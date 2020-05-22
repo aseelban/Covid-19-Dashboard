881b52c83689442c5aa9f8eea8fc945a22603482
aseelban <aseelban@gmail.com>
2020-05-22 06:48:43 +0300

    05

--- src/component/TabelGlobal.jsx
+++ src/component/TabelGlobal.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { MDBDataTable } from 'mdbreact';
 import { Container,Col, Row} from "mdbreact";
 import './TabelGlobal.css'
+import NumberFormat from 'react-number-format';
+
 
 
 const TabelGlobal = (props) => {
@@ -9,16 +11,21 @@ const TabelGlobal = (props) => {
   const {data} = props;
 
  
+  
+  const convert = (number) => {
+    
+    return <NumberFormat value={number} displayType={'text'} thousandSeparator={true} />
+  }
 
     let get_data = data.map((e) => {
       return ({
-        id: e["#"],
+        id: convert(e["#"]),
         country: e.Country,
-        confirmed: e.TotalCases,
-        recovered: e.TotalRecovered,
-        deaths: e.TotalDeaths,
-        seriousCritical: e.Serious_Critical,
-        activeCases: e.ActiveCases,
+        confirmed: convert(e.TotalCases),
+        recovered: convert(e.TotalRecovered),
+        deaths: convert(e.TotalRecovered),
+        seriousCritical: convert(e.Serious_Critical),
+        activeCases: convert(e.ActiveCases),
         
       })
     })
